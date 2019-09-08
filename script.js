3bfd5556a7b40627a72478c54f39f686c1308db3
Bobby Joe Smith III <bsmith07@risd.edu>
2019-09-08 18:22:54 -0400

    Clean up code

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
 var prev_media_index;
 
-// var media_lib = [
-//     ['<a href="http://images.nintendolife.com/news/2013/01/nintendo_comes_out_on_top_in_consumer_experience_poll/attachment/0/large.jpg" target="_blank"><img src="http://images.nintendolife.com/news/2013/01/nintendo_comes_out_on_top_in_consumer_experience_poll/attachment/0/large.jpg" class="media_object" alt="kid and computer"></a>', '<a href="http://images.nintendolife.com/news/2013/01/nintendo_comes_out_on_top_in_consumer_experience_poll/attachment/0/large.jpg" class="note_object" target="_blank">http://images.nintendolife.com/news/2013/01/nintendo_comes_out_on_top_in_consumer_experience_poll/attachment/0/large.jpg</a>'],
-//     ['<a href="https://r.hswstatic.com/w_907/gif/podcasts/missedinhistory-podcasts-wp-content-uploads-sites-99-2016-06-Bayard-Rustin-1-P-Full.jpg"  target="_blank"><img src="https://r.hswstatic.com/w_907/gif/podcasts/missedinhistory-podcasts-wp-content-uploads-sites-99-2016-06-Bayard-Rustin-1-P-Full.jpg" class="media_object" alt="Bayard Rustin"></a>', '<a href="https://r.hswstatic.com/w_907/gif/podcasts/missedinhistory-podcasts-wp-content-uploads-sites-99-2016-06-Bayard-Rustin-1-P-Full.jpg" class="note_object" target="_blank">https://r.hswstatic.com/w_907/gif/podcasts/missedinhistory-podcasts-wp-content-uploads-sites-99-2016-06-Bayard-Rustin-1-P-Full.jpg</a>']
-// ]
-
-// // Display media in media display section when user hovers over associated word
-// $(".media_trigger").mouseenter(function() {
-//
-//     var media_index = $(this).attr("data-media-index");
-//     media_index = parseInt(media_index);
-//     // note: remove on deployment
-//     console.log(media_index);
-//     // PEE PEE PANTS CASA DI POOPI
-//     // Keep from reposting the same media if already displayed
-//     if (prev_media_index !== media_index) {
-//         $(".media_object").replaceWith(media_lib[media_index][0]);
-//         $(".note_object").replaceWith(media_lib[media_index][1]);
-//     }
-//     prev_media_index = media_index;
-// })
-
-var media_lib = {
-    media_index: "00.1"
-}
-
 function mediaConfig(type, url, alt) {
     let htmlString;
     if (type == "image") {
@@ -53,8 +28,7 @@ $(".media_trigger").mouseenter(function() {
     let media_alt = $(this).attr("data-media-alt");
     let media_source = $(this).attr("data-media-source");
     let media_note = $(this).attr("data-media-note");
-    // type, url, alt
-    // mediaConfig(media_type, media_url, media_alt);
+
     // Keep from reposting the same media if already displayed
     if (prev_media_index !== media_index) {
         $(".media_object").replaceWith(mediaConfig(media_type, media_url, media_alt));
@@ -66,8 +40,6 @@ $(".media_trigger").mouseenter(function() {
 
 // Navigation scrolling
 let navLinks = document.querySelectorAll("nav a");
-
-
 
 // Check which part of document we are on when page loads
 checkCurrent($(".content"));
